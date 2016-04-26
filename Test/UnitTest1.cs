5538e3ba65dee69fae587d55dba49fa849aebf0e
Irishka256 <Irishka256@mail.ru>
2016-04-26 22:26:58 +0700

    Реализация теста для метода загрузки вопросов

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab;
 
 namespace Test
 {
     [TestClass]
     public class UnitTest1
     {
+        Game game = new Game();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestGameLoadQuestions()
         {
+            game.LoadQuestions();
+            int realSize = (game.Questions != null ? game.Questions.Count : 0);
+            int needMinimalSize = 10;
+
+            if (realSize <= needMinimalSize)
+            {
+                Assert.Fail("Вопросы не загружены. Загружено: {0}. Требуется: >= {1}", realSize, needMinimalSize);
+            }
         }
     }
 }
